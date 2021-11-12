b60c9f361835d5e10243791f99e9ff4e063e116b
Ricardo M <a21310359@ceti.mx>
2021-11-11 21:51:02 -0600

    iniciando proyecto de suma

---
+++ calculadora test.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int main()
+{
+    int n, n2, r;
+
+    printf("Introduce el numero 1: ")
+    scanf("%i",&n);
+
+    printf("Introduce el numero 2:");
+    scanf("%i",&n2);
+}
